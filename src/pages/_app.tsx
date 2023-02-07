db4e13cb2ca284d386a67ec97ac7b11980ae5eb5
Shirahamah <hiropi1224s@gmail.com>
2023-02-07 22:25:34 +0900

    🎉

---
+++ src/pages/_app.tsx
@@ -0,0 +1,30 @@
+import { MantineProvider } from '@mantine/core';
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+
+export default function App(props: AppProps): JSX.Element {
+  const { Component, pageProps } = props;
+
+  return (
+    <>
+      <Head>
+        <title>Mantine dev</title>
+        <meta
+          name='viewport'
+          content='minimum-scale=1, initial-scale=1, width=device-width'
+        />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: 'light',
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next';
+
+export const Home: NextPage = () => {
+  return <>hello</>;
+};
+
+export default Home;
